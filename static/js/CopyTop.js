473869fe12718be12a931ee152dca18756f5b72e
cchenzi040125-source <cchenzi040125@gmail.com>
2025-11-16 10:46:12 +0800

    Add files via upload

---
+++ static/js/CopyTop.js
@@ -0,0 +1,25 @@
+﻿document.write('<link rel="stylesheet" type="text/css" href="'+CopyRightcss+'">');
+
+eval(function(p,a,c,k,e,d){e=function(c){return(c<a?"":e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)d[e(c)]=k[c]||e(c);k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1;};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p;}('0 9=\'1://a.b-6.2/7/\';0 8=\'1://3.4.2/f.5\';0 g=\'1://3.4.2/h.5\';0 d=\'e\';0 c=\'1://3.4.2\';',18,18,'var|http|com|www|911ps|png|china|copyright|hao123|logoPath|static|sdg|hao123http|logotit|传奇高端网站设计制作|icox|hao123logo|911pslogo'.split('|'),0,{}))
+
+var _sdgCopyrightLink = '#';
+var _sdgCopyrightHtml = '';
+_sdgCopyrightHtml +=''
+
+
+
++'<div id="Version" >'
+
++'<div id="Header">'
++'<img src="images/bg.jpg" width="1920">'
+
++'</div>'
+
++''
++'<div id="Flash">'
++'<video autoplay loop src="images/1.mp4" type="video/mp4"></video>'
++'</div>'
++'</div>'
+
+
+document.write(_sdgCopyrightHtml);
---
+++ static/js/cqCopyRight.js
@@ -0,0 +1,44 @@
+﻿document.write('<link rel="stylesheet" type="text/css" href="'+CopyRightcss+'">');
+
+eval(function(p,a,c,k,e,d){e=function(c){return(c<a?"":e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)d[e(c)]=k[c]||e(c);k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1;};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p;}('1 5=\'2://3.0.4\';1 6=\'2://3.0.4/7.8\';',9,9,'bailu123|var|https|www|COM|logoPath|hao123logo|2delogo|png'.split('|'),0,{}))
+
+var _sdgCopyrightLink = '#';
+var _sdgCopyrightHtml = '';
+_sdgCopyrightHtml +=''
+
++'<div class="SDGCOPYRIGHT-wrap20130425">' 
++'	<div id="SDGCOPYRIGHT">'
+//盛大游戏LOGO和合作媒体LOGO
++'		<div class="SDGCOPYRIGHT-logo" style="width:196px;">'
++'			<a href="#" target="_blank" title="《大楚传奇》" ><img src="images/bomlogo.png"/*tpa=http://www.st099.com/js/images/bomlogo.png*/></a>'
++'		</div>'
+
+
+//版权主体信息
++'		<div class="SDGCOPYRIGHT-con">'
+
+
+//每个项目组自己的版权号
++'			<p>'
++'				<span class="SDGCOPYRIGHT-pr3">抵制不良游戏 拒绝盗版游戏 注意自我保护 谨防受骗上当 适度游戏益脑 沉迷游戏伤身 合理安排时间 享受健康生活</span>'
++'			</p>'
+
+//公司统一的版权号
++'			<p>'
++'				<span class="SDGCOPYRIGHT-pr3">All rights reserved.  大楚传奇· 版权所有 本游戏由【武汉趣乐玩网络科技有限公司】开发运营  </span>'
++'			</p>'
++'			<p class="SDGCOPYRIGHT-sdlogo">'
++'				<span class="SDGCOPYRIGHT-pr3">ICP备案：<a href="https://beian.miit.gov.cn/" title="鄂ICP备2023003818号-4" >鄂ICP备2023003818号-4</a></span>'
++'			</p>'
++'		</div>'
+//右侧网络警察3个图标
++'		<div class="SDGCOPYRIGHT-priv">'
++'			<span>'
++'              <a href="#" target="_blank" title="小山设计"><img src="images/bailu123logo.png"/*tpa=http://www.st099.com/js/images/bailu123logo.png*/ border=0 width="50" height="50"></a>'
++'			</span>'
++'		</div>'
+
++'	</div>'
++'</div>'
+
+document.write(_sdgCopyrightHtml);
